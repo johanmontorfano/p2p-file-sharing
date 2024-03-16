51a173122241023a238578c292cc8f1fbea1b9cf
Johan Montorfano <me@johanmontorfano.com>
2024-03-16 19:13:31 +0100

    added error displaying

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,19 @@ import {Broadcasting} from './broadcasting';
 import "./index.css";
 import Peer from 'peerjs';
 import {Receiving} from './receiving';
+import {createSignal} from 'solid-js';
 
 function App() {
+    const [error, setError] = createSignal("");
     const peer = new Peer(v4(), {debug: 3});
+
+    peer.on("error", (err) => setError(err.message));
 
     return <div class="w-full flex justify-center">
         <div class="max-w-[800px] w-full p-4">
             <h1>P2P File Sharing</h1>
             <p>Identity: <strong>{peer.id}</strong></p>
+            <p class="text-red">{error()}</p>
             <br />
             <Accordion defaultActiveKey="1">
                 <Accordion.Item eventKey="0">
