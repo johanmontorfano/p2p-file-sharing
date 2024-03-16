0ac1a10ee5efb3259ce471234a074ff2d121722d
Johan Montorfano <me@johanmontorfano.com>
2024-03-17 00:40:10 +0100

    added errors/warnings

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,47 @@
 /* @refresh reload */
-import { Accordion } from 'solid-bootstrap';
+import { Accordion, Card } from 'solid-bootstrap';
 import { render } from 'solid-js/web';
 import { v4 } from "uuid";
 import {Broadcasting} from './broadcasting';
 import "./index.css";
 import Peer from 'peerjs';
 import {Receiving} from './receiving';
-import {createSignal} from 'solid-js';
+import {Show, createSignal} from 'solid-js';
 
 function App() {
     const [error, setError] = createSignal("");
-    const peer = new Peer(v4(), {debug: 3});
+    const peer = new Peer(v4());
 
-    peer.on("error", (err) => setError(err.message));
+    peer.on("error", (err) => setError(err.message + " Try to refresh."));
 
     return <div class="w-full flex justify-center">
         <div class="max-w-[800px] w-full p-4">
             <h1>P2P File Sharing</h1>
             <p>Identity: <strong>{peer.id}</strong></p>
-            <p class="text-red">{error()}</p>
+            <br />
+            <Show when={error() !== ""}>
+                <Card bg="danger" text="white">
+                    <Card.Header>
+                        Critical Error
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>{error()}</Card.Text>
+                    </Card.Body>
+                </Card>
+            </Show>
+            <br />
+            <Card bg="warning" text="white">
+                <Card.Header>
+                    Information
+                </Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        The is a known issue about socket connections not working
+                        on Chromium-based browsers (sometimes) and Safari. This 
+                        is abnormal and a patch will soon be published.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
             <br />
             <Accordion defaultActiveKey="1">
                 <Accordion.Item eventKey="0">
